ea25ba4bfff59a34f426f14d5abdea654486781a
Isidro Fuentes <isidrofuenteshermoso@gmail.com>
2026-05-20 11:51:49 +0200

    Added profile parameters

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -3,14 +3,35 @@
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 # Check if directory exists, create if it doesn't
-if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --zosmf-profile mf &>/dev/null; then
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" \
+  --host ${{ secrets.ZOWE_HOST }} \
+  --port ${{ secrets.ZOWE_PORT }} \
+  --user ${{ secrets.ZOWE_USERNAME }} \
+  --password ${{ secrets.ZOWE_PASSWORD }} \
+  --reject-unauthorized false &>/dev/null; then
   echo "Directory does not exist. Creating it..."
-  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck --zosmf-profile mf
+  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck \
+    --host ${{ secrets.ZOWE_HOST }} \
+    --port ${{ secrets.ZOWE_PORT }} \
+    --user ${{ secrets.ZOWE_USERNAME }} \
+    --password ${{ secrets.ZOWE_PASSWORD }} \
+    --reject-unauthorized false
 else
   echo "Directory already exists."
 fi
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files --zosmf-profile mf "cobol-check-0.2.19.jar"
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files \
+  --host ${{ secrets.ZOWE_HOST }} \
+  --port ${{ secrets.ZOWE_PORT }} \
+  --user ${{ secrets.ZOWE_USERNAME }} \
+  --password ${{ secrets.ZOWE_PASSWORD }} \
+  --reject-unauthorized false \
+  "cobol-check-0.2.19.jar"
 # Verify upload
 echo "Verifying upload:"
-zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --zosmf-profile mf
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" \
+  --host ${{ secrets.ZOWE_HOST }} \
+  --port ${{ secrets.ZOWE_PORT }} \
+  --user ${{ secrets.ZOWE_USERNAME }} \
+  --password ${{ secrets.ZOWE_PASSWORD }} \
+  --reject-unauthorized false
